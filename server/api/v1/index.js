29c0926e8700121a57c7965a693d62bd50b4f62a
Mukul Shakya <muklshakya123@gmail.com>
2020-09-13 11:10:33 +0530

    moved server

--- server/api/v1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = new express.Router();
-
-router.use(require("./routes/user"));
-router.use(require("./routes/product"));
-router.use(require("./routes/upload"));
-
-module.exports = router;
--- server/api/v1/routes/upload.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = new express.Router();
-const { upload } = require("../controllers");
-const auth = require("../../../middleware/auth");
-
-router.route("/upload").post(auth, upload.uploadFile);
-
-module.exports = router;
