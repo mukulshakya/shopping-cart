be9b16c121a3069147ec2eef050ab2aeeffad5eb
Mukul Shakya <muklshakya123@gmail.com>
2021-02-11 13:38:33 +0530

    Updates

---
+++ precache-manifest.9887e66f1abfaffe177283dadf87cdae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee82d64a51018f19999840f71e18a364",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "9b2e8a92de4a0021671d",
+    "url": "/shopping-cart/static/css/2.78424409.chunk.css"
+  },
+  {
+    "revision": "b5953b79caf8307707cf",
+    "url": "/shopping-cart/static/css/main.8d04a982.chunk.css"
+  },
+  {
+    "revision": "9b2e8a92de4a0021671d",
+    "url": "/shopping-cart/static/js/2.40573195.chunk.js"
+  },
+  {
+    "revision": "4d7749ba1ea6a5029026f2621a685668",
+    "url": "/shopping-cart/static/js/2.40573195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5953b79caf8307707cf",
+    "url": "/shopping-cart/static/js/main.776eda14.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
